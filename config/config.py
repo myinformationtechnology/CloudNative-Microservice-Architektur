4b72169551ebca794d9853082e3cade5ba4352bd
ehsan Keshavarzi <myinformationtechnolgy.369@gmail.com>
2023-11-02 20:21:45 +0100

    Hinzufügen von Ressourcen und Controllern für die Authentifizierung per JWT-Token

--- config/config.py
+++ config/config.py
@@ -1,31 +1,31 @@
-from os import environ
 
-
+```python
 class Config:
-    ######################### global Configuration #########################
+    ######################### Globale Konfiguration #########################
     ENV = environ.get("TOYBOX_AYTHZ_ENV", "production")
     DEBUG = bool(int(environ.get("TOYBOX_AYTHZ_DEBUG", "0")))
     TESTING = bool(int(environ.get("TOYBOX_AYTHZ_TESTING", "0")))
-    # at least 256 chars and complex for production
-    SECRET_KEY = environ.get("TOYBOX_AUTHZ_SECRET_KEY", "HARD_STRONG_SECRET_KEY")
+    # Mindestens 256 Zeichen und komplex für die Produktion
+    SECRET_KEY = environ.get("TOYBOX_AUTHZ_SECRET_KEY", "STARKES_GEHEIMES_SCHLÜSSELWORT")
     JSONIFY_PRETTYPRINT_REGULAR = True
     TIMEZONE = environ.get("TOYBOY_AUTHZ_TIMEZONE", "Asia/Tehran")
 
-    ######################### Database Configuration #########################
+    ######################### Datenbankkonfiguration #########################
     SQLALCHEMY_DATABASE_URI = environ.get("TOYBOY_AUTHZ_DATABASE_URI", None)
     SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
 
-    ########################### User Configuration ###########################
+    ########################### Benutzerkonfiguration ###########################
     USER_DEFAULT_EXPIRES_TIME = int(
         environ.get("TOYBOX_AUTHZ_USER_DEFAULT_EXPIRES_TIME", "365")
     )
-    USER_DEFAULT_ROLE = environ.get("TOYBOX_AUTHZ_USER_DEFAULT_ROLE", "member")
+    USER_DEFAULT_ROLE = environ.get("TOYBOX_AUTHZ_USER_DEFAULT_ROLE", "Mitglied")
     USER_DEFAULT_STATUS = int(environ.get("TOYBOX_AUTHZ_USER_DEFAULT_STATUS", "0"))
 
-    ####################### Authentication Configuration ######################
+    ####################### Authentifizierungskonfiguration ######################
     JWT_TOKEN_DEFAULT_EXPIRY_TIME = int(
         environ.get("TOYBOX_AUTHZ_JWT_TOKEN_DEFAULT_EXPIRY_TIME", "86400")
     )
     JWT_TOKEN_DEFAULT_ALGORITHM = environ.get(
         "TOYBOX_AUTHZ_JWT_TOKEN_DEFAULT_ALGORITHM", "HS512"
     )
+```
