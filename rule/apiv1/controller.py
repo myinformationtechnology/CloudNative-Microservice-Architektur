b1e56e868ccadd2f9ab2a535ad5ddd650fb60480
Ehsan <myinformationtechnology369@gmail.com>
2023-12-28 22:45:22 +0100

    Kommentare zur Klasse ControllerAccessRules hinzugefügt

--- rule/apiv1/controller.py
+++ rule/apiv1/controller.py
@@ -1,4 +1,8 @@
-class ControllerAccessRules:
+
+    class ControllerAccessRules:
+    # Dieses private Dictionary definiert die Zugriffsregeln für verschiedene Funktionen im System.
+    # Die Schlüssel sind die Namen der Funktionen und die Werte sind Listen von Rollen, 
+    # die Zugriff auf diese Funktionen haben.
     __Controller_allowed_rules = {
         "get_user_list": ["admin", "service"],
         "get_user": ["admin", "service", "member:user_id"],
@@ -7,5 +11,8 @@ class ControllerAccessRules:
         "delete_user": ["admin"],
     }
 
+    # Diese Methode gibt die erlaubten Rollen für eine gegebene Funktion zurück.
+    # Sie nimmt den Namen der Funktion als Parameter und gibt die entsprechende Liste von erlaubten Rollen zurück.
     def get_controller_allowed_rules(f):
         return ControllerAccessRules.__Controller_allowed_rules[f]
+
