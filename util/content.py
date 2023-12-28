ac97d945f4a0883887b4b42bfea388cf9244732c
Ehsan <myinformationtechnology369@gmail.com>
2023-12-28 19:38:58 +0100

    Verstärke Sicherheitsdokumentation in content.py für bessere Klarheit

--- util/content.py
+++ util/content.py
@@ -1,8 +1,25 @@
+
+    # Importieren der notwendigen Module aus dem Flask-Framework und projektinternen Hilfsfunktionen
 from flask import Flask, request
 from authz.util import jsonify
 from authz.config import Config
 
-
 def check_request_content_type():
+    """
+    Funktion zur Überprüfung des Content-Types der eingehenden Anfragen.
+    Stellt sicher, dass alle Anfragen den Content-Type 'application/json' haben.
+    
+    Returns:
+        - JSON-Antwort mit Statuscode 415 (Unsupported Media Type), 
+          wenn der Content-Type nicht 'application/json' ist.
+        - None, wenn der Content-Type korrekt ist, um die weitere Verarbeitung zu ermöglichen.
+    """
+    # Überprüfung des Content-Types der Anfrage
     if request.content_type != "application/json":
+        # Rückgabe einer JSON-Antwort mit Statuscode 415, wenn der Content-Type nicht korrekt ist
         return jsonify(status=415, code=101)
+    # Keine Aktion erforderlich, wenn der Content-Type korrekt ist
+    return None
+
+# Hier können weitere Funktionen oder Konfigurationen für die Flask-Anwendung hinzugefügt werden
+
