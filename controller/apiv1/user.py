de5e280e4a441fcd3292c5b80e0360852b83944d
Ehsan <myinformationtechnology369@gmail.com>
2023-12-28 17:19:16 +0100

    Überarbeite user.py für verbesserte Lesbarkeit und Struktur

--- controller/apiv1/user.py
+++ controller/apiv1/user.py
@@ -1,94 +1,72 @@
+from flask import request
 from authz.authz import db
-from authz.decorator.apiv1 import auth_required
 from authz.model import User
 from authz.schema.apiv1 import UserSchema
-from authz.util import jsonify, now
-from flask import request
+from authz.util import jsonify
+from authz.decorator.apiv1 import auth_required
 
 class UserController:
-    @auth_required
-    def get_user_list():
-        try:
-            users = User.query.all()  # Abfrage von Benutzern aus der Datenbank
-        except:
-            return jsonify(status=500, code=102)
-        users_schema = UserSchema(many=True)  # Erstellung eines Serialisierungsobjekts für die Liste der Benutzer
-        return jsonify({"Users": users_schema.dump(users)})  # Rückgabe der Liste der Benutzer
+    def __init__(self):
+        # Initialisierung der UserSchema-Instanzen für einzelne und mehrere Benutzer.
+        self.user_schema = UserSchema()
+        self.user_list_schema = UserSchema(many=True)
 
     @auth_required
-    def get_user(user_id):
-        try:
-            user = User.query.get(user_id)  # Abrufen eines Benutzers
-        except:
-            return jsonify(status=500, code=102)  # Datenbankfehler
-        if user is None:
-            return jsonify(status=404, code=103)  # Benutzer wurde nicht gefunden
-        user_schema = UserSchema()
-        return jsonify({"User": user_schema.dump(user)})  # Rückgabe eines Benutzers
+    def get_user_list(self):
+        # Abruf aller Benutzer aus der Datenbank und Rückgabe als JSON.
+        users = User.query.all()
+        return jsonify({"Users": self.user_list_schema.dump(users)})
 
-    def create_user():
-        user_schema = UserSchema(only=["username", "password"])
-        try:
-            data = user_schema.load(request.get_json())  # Validierung der Benutzerdaten
-        except:
-            return jsonify(status=400, code=104)  # Validierung fehlgeschlagen
-        if not data["username"] or nicht data["password"]:  # Überprüfung auf nicht leere Felder
+    @auth_required
+    def get_user(self, user_id):
+        # Abruf eines einzelnen Benutzers anhand der Benutzer-ID.
+        user = User.query.get(user_id)
+        if not user:
+            # Rückgabe eines Fehlers, wenn der Benutzer nicht existiert.
+            return jsonify(status=404, code=103)
+        return jsonify({"User": self.user_schema.dump(user)})
+
+    def create_user(self):
+        # Erfassung und Validierung der Benutzerdaten aus der Anfrage.
+        data = request.get_json()
+        if not data or not data.get("username") or not data.get("password"):
+            # Rückgabe eines Fehlers bei ungültigen Eingabedaten.
             return jsonify(status=400, code=105)
-        try:
-            user = User.query.filter_by(username=data["username"]).first()  # Überprüfen auf Benutzernamenduplikate
-        except:
-            return jsonify(status=500, code=102)  # Datenbankfehler
-        if user is not None:
-            return jsonify(status=409, code=106)  # Ressourcenkonflikt
-        user = User(username=data["username"], password=data["password"])
-        db.session.add(user)  # Auswahl des Benutzers zur Einfügung in die Datenbank
-        try:
-            db.session.commit()  # Einfügen des Benutzers in die Datenbank
-        except:
-            db.session.rollback()  # Rollback im Falle eines Datenbankfehlers
-            return jsonify(status=500, code=102)  # Datenbankfehler
-        user_schema = UserSchema()
-        return jsonify({"User": user_schema.dump(user)}, status=201)  # Rückgabe eines neuen Benutzers
+
+        # Überprüfung, ob der Benutzername bereits existiert.
+        if User.query.filter_by(username=data["username"]).first():
+            return jsonify(status=409, code=106)
+
+        # Erstellung und Speicherung eines neuen Benutzers.
+        new_user = User(username=data["username"], password=data["password"])
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"User": self.user_schema.dump(new_user)}, status=201)
 
     @auth_required
-    def update_user(user_id):
-        user_schema = UserSchema(only=["password"])
-        try:
-            data = user_schema.load(request.get_json())  # Validierung der Benutzerdaten
-        except:
-            return jsonify(status=400, code=104)  # Validierung fehlgeschlagen
-        if not data["password"]:  # Überprüfung auf nicht leeres Feld
-            return jsonify(status=400, code=105)
-        try:
-            user = User.query.get(user_id)  # Auswahl des Benutzers
-        except:
-            return jsonify(status=500, code=102)  # Datenbankfehler
-        if user is None:
-            return jsonify(status=404, code=103)  # Benutzer wurde nicht gefunden
-        user.password = data["password"]
-        user.last_change_at = now()
-        db.session.add(user)
-        try:
-            db.session.commit()  # Aktualisieren des Benutzerpassworts in der Datenbank
-        except:
-            db.session.rollback()
-            return jsonify(status=500, code=102)
-        user_schema = UserSchema()
-        return jsonify({"user": user_schema.dump(user)})
+    def update_user(self, user_id):
+        # Aktualisierung der Benutzerdaten.
+        user = User.query.get(user_id)
+        if not user:
+            # Rückgabe eines Fehlers, wenn der Benutzer nicht existiert.
+            return jsonify(status=404, code=103)
+
+        data = request.get_json()
+        user.password = data.get("password", user.password)
+        db.session.commit()
+        return jsonify({"User": self.user_schema.dump(user)})
 
     @auth_required
-    def delete_user(user_id):
-        try:
-            user = User.query.get(user_id)  # Auswahl des Benutzers
-        except:
-            return jsonify(status=500, code=102)  # Datenbankfehler
-        if user is None:
-            return jsonify(status=404, code=103)  # Benutzer wurde nicht gefunden
+    def delete_user(self, user_id):
+        # Löschen eines Benutzers anhand der Benutzer-ID.
+        user = User.query.get(user_id)
+        if not user:
+            # Rückgabe eines Fehlers, wenn der Benutzer nicht existiert.
+            return jsonify(status=404, code=103)
+
         db.session.delete(user)
-        try:
-            db.session.commit()  # Aktualisieren des Benutzerpassworts in der Datenbank
-        except:
-            db.session.rollback()
-            return jsonify(status=500, code=102)
-        return jsonify()
+        db.session.commit()
+        return jsonify(status=200)
+
+
 
