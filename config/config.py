27f297841dc177f470d366247e32fa262cca1e85
Ehsan <myinformationtechnology369@gmail.com>
2023-12-31 15:20:51 +0100

    Aktualisierung der Config-Klasse: Kommentare hinzugefügt und Validierungslogik implementiert

--- config/config.py
+++ config/config.py
@@ -1,31 +1,31 @@
+from os import environ
 
-```python
 class Config:
-    ######################### Globale Konfiguration #########################
-    ENV = environ.get("TOYBOX_AYTHZ_ENV", "production")
-    DEBUG = bool(int(environ.get("TOYBOX_AYTHZ_DEBUG", "0")))
-    TESTING = bool(int(environ.get("TOYBOX_AYTHZ_TESTING", "0")))
-    # Mindestens 256 Zeichen und komplex für die Produktion
-    SECRET_KEY = environ.get("TOYBOX_AUTHZ_SECRET_KEY", "STARKES_GEHEIMES_SCHLÜSSELWORT")
-    JSONIFY_PRETTYPRINT_REGULAR = True
-    TIMEZONE = environ.get("TOYBOY_AUTHZ_TIMEZONE")
+    # Globale Konfiguration
+    # Umgebungsvariablen werden verwendet, um die Konfiguration flexibel zu gestalten
+    ENV = environ.get("TOYBOX_AUTHZ_ENV", "production")  # Standardmäßig 'production'
+    DEBUG = environ.get("TOYBOX_AUTHZ_DEBUG", "0") == "1"  # Aktiviert Debug-Modus wenn '1'
+    TESTING = environ.get("TOYBOX_AUTHZ_TESTING", "0") == "1"  # Aktiviert Test-Modus wenn '1'
 
-    ######################### Datenbankkonfiguration #########################
-    SQLALCHEMY_DATABASE_URI = environ.get("TOYBOY_AUTHZ_DATABASE_URI", None)
-    SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
+    # Sicherheitsschlüssel für die Anwendung
+    SECRET_KEY = environ.get("TOYBOX_AUTHZ_SECRET_KEY")
+    if not SECRET_KEY:
+        raise ValueError("Kein SECRET_KEY für die Toybox-Authz-Konfiguration festgelegt")
 
-    ########################### Benutzerkonfiguration ###########################
-    USER_DEFAULT_EXPIRES_TIME = int(
-        environ.get("TOYBOX_AUTHZ_USER_DEFAULT_EXPIRES_TIME", "365")
-    )
-    USER_DEFAULT_ROLE = environ.get("TOYBOX_AUTHZ_USER_DEFAULT_ROLE", "Mitglied")
-    USER_DEFAULT_STATUS = int(environ.get("TOYBOX_AUTHZ_USER_DEFAULT_STATUS", "0"))
+    JSONIFY_PRETTYPRINT_REGULAR = True  # Schöne Formatierung von JSON-Antworten
+    TIMEZONE = environ.get("TOYBOX_AUTHZ_TIMEZONE", "UTC")  # Standardzeitzone ist UTC
 
-    ####################### Authentifizierungskonfiguration ######################
-    JWT_TOKEN_DEFAULT_EXPIRY_TIME = int(
-        environ.get("TOYBOX_AUTHZ_JWT_TOKEN_DEFAULT_EXPIRY_TIME", "86400")
-    )
-    JWT_TOKEN_DEFAULT_ALGORITHM = environ.get(
-        "TOYBOX_AUTHZ_JWT_TOKEN_DEFAULT_ALGORITHM", "HS512"
-    )
-```
+    # Datenbankkonfiguration
+    SQLALCHEMY_DATABASE_URI = environ.get("TOYBOX_AUTHZ_DATABASE_URI")
+    if not SQLALCHEMY_DATABASE_URI:
+        raise ValueError("Kein DATABASE_URI für die Toybox-Authz-Konfiguration festgelegt")
+    SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG  # Trackt Modifikationen im Debug-Modus
+
+    # Benutzerkonfiguration
+    USER_DEFAULT_EXPIRES_TIME = int(environ.get("TOYBOX_AUTHZ_USER_DEFAULT_EXPIRES_TIME", "365"))  # Standard-Ablaufzeit für Benutzer
+    USER_DEFAULT_ROLE = environ.get("TOYBOX_AUTHZ_USER_DEFAULT_ROLE", "Mitglied")  # Standardrolle für neue Benutzer
+    USER_DEFAULT_STATUS = int(environ.get("TOYBOX_AUTHZ_USER_DEFAULT_STATUS", "0"))  # Standardstatus für neue Benutzer
+
+    # Authentifizierungskonfiguration
+    JWT_TOKEN_DEFAULT_EXPIRY_TIME = int(environ.get("TOYBOX_AUTHZ_JWT_TOKEN_DEFAULT_EXPIRY_TIME", "86400"))  # Standard-Ablaufzeit für JWT-Tokens
+    JWT_TOKEN_DEFAULT_ALGORITHM = environ.get("TOYBOX_AUTHZ_JWT_TOKEN_DEFAULT_ALGORITHM", "HS512")  # Standard-Algorithmus für JWT-Tokens
