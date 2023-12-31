9bbcec0d66c476d697ddc196756583e83241cac9
Ehsan <myinformationtechnology369@gmail.com>
2023-12-31 13:59:29 +0100

    Erweiterung von auth.py mit JWT-basierter AuthTokenResource für sichere Token-Handhabung

--- resource/apiv1/auth.py
+++ resource/apiv1/auth.py
@@ -1,16 +1,29 @@
 from flask_restful import Resource
 from authz.controller.apiv1 import AuthTokenController
+from flask import request, jsonify
 
 class AuthTokenResource(Resource):
     def get(self):
         """
-        GET /auth/tokens --> Benutzer JWT-Token überprüfen
+        GET /auth/tokens
+        Überprüft das JWT-Token des Benutzers und gibt den Validierungsstatus zurück.
         """
-        return AuthTokenController.verify_jwt_token()  # Benutzer JWT-Token überprüfen
+        token = request.headers.get('Authorization')
+        valid, response = AuthTokenController.verify_jwt_token(token)
+        if valid:
+            return jsonify(response), 200
+        else:
+            return jsonify(response), 401
 
     def post(self):
         """
-        POST /auth/tokens --> Benutzer JWT-Token erstellen
+        POST /auth/tokens
+        Erstellt ein JWT-Token für einen authentifizierten Benutzer.
         """
-        return AuthTokenController.create_jwt_token()  # Benutzer JWT-Token erstellen
+        user_data = request.json
+        token, response = AuthTokenController.create_jwt_token(user_data)
+        if token:
+            return jsonify({'token': token}), 201
+        else:
+            return jsonify(response), 400
 
