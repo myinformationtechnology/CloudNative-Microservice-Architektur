590c9116b562d56500380cf33e9050a7d7a7d8b0
Ehsan <myinformationtechnology369@gmail.com>
2023-12-31 14:29:35 +0100

    Erweiterte Fehlerbehandlung und detaillierte Kommentare in AuthTokenResource hinzugefügt

--- resource/apiv1/auth.py
+++ resource/apiv1/auth.py
@@ -7,9 +7,21 @@ class AuthTokenResource(Resource):
         """
         GET /auth/tokens
         Überprüft das JWT-Token des Benutzers und gibt den Validierungsstatus zurück.
+        Extrahiert das Token aus dem Authorization-Header und validiert es.
+        Gibt bei erfolgreicher Validierung eine positive Antwort zurück,
+        sonst eine Fehlermeldung.
         """
+        # Extrahiert das Token aus dem Authorization-Header
         token = request.headers.get('Authorization')
+
+        # Überprüft, ob ein Token vorhanden ist
+        if not token:
+            return jsonify({'message': 'Authorization token is missing'}), 401
+
+        # Validiert das Token
         valid, response = AuthTokenController.verify_jwt_token(token)
+
+        # Gibt entsprechende Antwort basierend auf der Validierung zurück
         if valid:
             return jsonify(response), 200
         else:
@@ -19,11 +31,25 @@ class AuthTokenResource(Resource):
         """
         POST /auth/tokens
         Erstellt ein JWT-Token für einen authentifizierten Benutzer.
+        Nimmt Benutzerdaten aus dem Request-Body und generiert ein JWT-Token.
+        Gibt das Token bei erfolgreicher Erstellung zurück,
+        sonst eine Fehlermeldung.
         """
+        # Extrahiert Benutzerdaten aus dem Request-Body
         user_data = request.json
+
+        # Überprüft, ob Benutzerdaten vorhanden sind
+        if not user_data:
+            return jsonify({'message': 'No data provided'}), 400
+
+        # Erstellt ein JWT-Token mit den Benutzerdaten
         token, response = AuthTokenController.create_jwt_token(user_data)
+
+        # Gibt das erstellte Token oder eine Fehlermeldung zurück
         if token:
             return jsonify({'token': token}), 201
         else:
             return jsonify(response), 400
+
+
 
