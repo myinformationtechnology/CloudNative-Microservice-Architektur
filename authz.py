e6c46b6a4a223186532c1be7cc05817f1bef6fb5
Ehsan <myinformationtechnology369@gmail.com>
2024-01-02 22:15:52 +0100

    Refaktorisiere Flask-App-Initialisierung und verbessere Logging

--- authz.py
+++ authz.py
@@ -3,28 +3,45 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
-from authz.util import check_request_content_type
+import logging  # Ersetzt 'print' durch das Logging-Modul
 
 from authz.config import Config
+from authz.util import check_request_content_type
 
+# Initialisierung der Flask-Erweiterungen
 db = SQLAlchemy()
 mg = Migrate()
 ma = Marshmallow()
 
+# Erstellung des Blueprints für die API
 apiv1_bp = Blueprint("apiv1", __name__, url_prefix="/api/v1")
 apiv1 = Api(apiv1_bp)
 
-from authz import resource  # Muss an dieser Stelle stehen, nach apiv1.
+# Import der Ressourcen; muss nach der Initialisierung von 'apiv1' erfolgen
+from authz import resource
 
 def create_app():
-    print("Starte")
+    """
+    Erstellt und konfiguriert die Flask-Anwendung.
+
+    Returns:
+        app: Die erstellte Flask-Anwendung.
+    """
+    # Verwenden des Logging-Moduls für bessere Nachrichtenverwaltung
+    logging.info("Starte die Flask-Anwendung")
+
     app = Flask(__name__)
-    app.config.from_object(Config)  # Lädt die Konfiguration aus Umgebungsvariablen.
-    app.before_request_funcs[None].append(
-        check_request_content_type
-    )  # Überprüft den Content-Type für jede Anfrage.
-    db.init_app(app)  # Initialisiert das SQLAlchemy-Datenbankobjekt.
-    mg.init_app(app, db)  # Initialisiert das Datenbankmanagement und das Migrate-Objekt.
-    ma.init_app(app)  # Initialisiert das Marshmallow-Objekt.
+    app.config.from_object(Config)  # Konfiguration aus der Config-Klasse laden
+
+    # Hinzufügen der Content-Type-Überprüfung als vorherige Anforderungsfunktion
+    app.before_request_funcs.setdefault(None, []).append(check_request_content_type)
+
+    # Initialisierung der Flask-Erweiterungen mit der App-Instanz
+    db.init_app(app)
+    mg.init_app(app, db)
+    ma.init_app(app)
+
+    # Registrierung des Blueprints bei der App
     app.register_blueprint(apiv1_bp)
+
     return app
